63c81e870bb4032e65883eeb31008cfb02a7e86b
Narayana Palla <narayana.palla@hotmail.com>
2018-09-16 21:41:00 -0400

    Added Phone, Company table scripts tests.

---
+++ Source/StoredProcsGeneratorTests/CompanyTableTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace StoredProcsGeneratorTests
+{
+    [TestClass]
+    public class CompanyTableTests : BaseTests
+    {
+        #region Company Update Tests
+        [TestMethod]
+        public void CompanyUpdateScriptTest()
+        {
+            var deleteScriptResult = CompanyUpdateDeleteScript();
+            Assert.AreEqual(-1, deleteScriptResult, "Company Update - Delete script result should match.");
+            var createScriptResult = ExecuteDdlStatement(GetSqlScriptText("update", "Company"));
+            Assert.AreEqual(-1, createScriptResult, "Company Update - Create script result should match.");
+        }
+
+        private int CompanyUpdateDeleteScript()
+        {
+            var deleteScript
+                            = @"IF EXISTS(SELECT * FROM sys.procedures WHERE name = 'usp_test_Company_Update')
+                                BEGIN
+	                            DROP PROCEDURE [dbo].[usp_test_Company_Update]
+                                END";
+            return ExecuteDdlStatement(deleteScript);
+        }
+
+        [TestMethod]
+        public void CompanyInsertScriptTest()
+        {
+            var deleteScriptResult = CompanyInsertDeleteScript();
+            Assert.AreEqual(-1, deleteScriptResult, "Company Insert - Delete script result should match.");
+            var createScriptResult = ExecuteDdlStatement(GetSqlScriptText("insert", "Company"));
+            Assert.AreEqual(-1, createScriptResult, "Company Insert - Create script result should match.");
+        }
+
+        private int CompanyInsertDeleteScript()
+        {
+            var deleteScript
+                            = @"IF EXISTS(SELECT * FROM sys.procedures WHERE name = 'usp_test_Company_Insert')
+                                BEGIN
+	                                DROP PROCEDURE [dbo].[usp_test_Company_Insert]
+                                END";
+            return ExecuteDdlStatement(deleteScript);
+        }
+
+        [TestMethod]
+        public void CompanyDeleteScriptTest()
+        {
+            var deleteScriptResult = CompanyDeleteDeleteScript();
+            Assert.AreEqual(-1, deleteScriptResult, "Company Delete - Delete script result should match.");
+            var createScriptResult = ExecuteDdlStatement(GetSqlScriptText("delete", "Company"));
+            Assert.AreEqual(-1, createScriptResult, "Company Delete - Create script result should match.");
+        }
+
+        private int CompanyDeleteDeleteScript()
+        {
+            var deleteScript
+                            = @"IF EXISTS(SELECT * FROM sys.procedures WHERE name = 'usp_test_Company_Delete')
+                                BEGIN
+	                                DROP PROCEDURE [dbo].[usp_test_Company_Delete]
+                                END";
+            return ExecuteDdlStatement(deleteScript);
+        }
+
+        [TestMethod]
+        public void CompanySearchScriptTest()
+        {
+            var deleteScriptResult = CompanySearchDeleteScript();
+            Assert.AreEqual(-1, deleteScriptResult, "Company Search - Delete script result should match.");
+            var createScriptResult = ExecuteDdlStatement(GetSqlScriptText("search", "Company"));
+            Assert.AreEqual(-1, createScriptResult, "Company Search - Create script result should match.");
+        }
+
+        private int CompanySearchDeleteScript()
+        {
+            var deleteScript
+                            = @"IF EXISTS(SELECT * FROM sys.procedures WHERE name = 'usp_test_Company_Search')
+                                BEGIN
+	                                DROP PROCEDURE [dbo].[usp_test_Company_Search]
+                                END";
+            return ExecuteDdlStatement(deleteScript);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/StoredProcsGeneratorTests/PhoneTableTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace StoredProcsGeneratorTests
+{
+    [TestClass]
+    public class PhoneTableTests : BaseTests
+    {
+        #region Phone Update Tests
+        [TestMethod]
+        public void PhoneUpdateScriptTest()
+        {
+            var deleteScriptResult = PhoneUpdateDeleteScript();
+            Assert.AreEqual(-1, deleteScriptResult, "Phone Update - Delete script result should match.");
+            var createScriptResult = ExecuteDdlStatement(GetSqlScriptText("update", "Phone"));
+            Assert.AreEqual(-1, createScriptResult, "Phone Update - Create script result should match.");
+        }
+
+        private int PhoneUpdateDeleteScript()
+        {
+            var deleteScript
+                            = @"IF EXISTS(SELECT * FROM sys.procedures WHERE name = 'usp_test_Phone_Update')
+                                BEGIN
+	                            DROP PROCEDURE [dbo].[usp_test_Phone_Update]
+                                END";
+            return ExecuteDdlStatement(deleteScript);
+        }
+
+        [TestMethod]
+        public void PhoneInsertScriptTest()
+        {
+            var deleteScriptResult = PhoneInsertDeleteScript();
+            Assert.AreEqual(-1, deleteScriptResult, "Phone Insert - Delete script result should match.");
+            var createScriptResult = ExecuteDdlStatement(GetSqlScriptText("insert", "Phone"));
+            Assert.AreEqual(-1, createScriptResult, "Phone Insert - Create script result should match.");
+        }
+
+        private int PhoneInsertDeleteScript()
+        {
+            var deleteScript
+                            = @"IF EXISTS(SELECT * FROM sys.procedures WHERE name = 'usp_test_Phone_Insert')
+                                BEGIN
+	                                DROP PROCEDURE [dbo].[usp_test_Phone_Insert]
+                                END";
+            return ExecuteDdlStatement(deleteScript);
+        }
+
+        [TestMethod]
+        public void PhoneDeleteScriptTest()
+        {
+            var deleteScriptResult = PhoneDeleteDeleteScript();
+            Assert.AreEqual(-1, deleteScriptResult, "Phone Delete - Delete script result should match.");
+            var createScriptResult = ExecuteDdlStatement(GetSqlScriptText("delete", "Phone"));
+            Assert.AreEqual(-1, createScriptResult, "Phone Delete - Create script result should match.");
+        }
+
+        private int PhoneDeleteDeleteScript()
+        {
+            var deleteScript
+                            = @"IF EXISTS(SELECT * FROM sys.procedures WHERE name = 'usp_test_Phone_Delete')
+                                BEGIN
+	                                DROP PROCEDURE [dbo].[usp_test_Phone_Delete]
+                                END";
+            return ExecuteDdlStatement(deleteScript);
+        }
+
+        [TestMethod]
+        public void PhoneSearchScriptTest()
+        {
+            var deleteScriptResult = PhoneSearchDeleteScript();
+            Assert.AreEqual(-1, deleteScriptResult, "Phone Search - Delete script result should match.");
+            var createScriptResult = ExecuteDdlStatement(GetSqlScriptText("search", "Phone"));
+            Assert.AreEqual(-1, createScriptResult, "Phone Search - Create script result should match.");
+        }
+
+        private int PhoneSearchDeleteScript()
+        {
+            var deleteScript
+                            = @"IF EXISTS(SELECT * FROM sys.procedures WHERE name = 'usp_test_Phone_Search')
+                                BEGIN
+	                                DROP PROCEDURE [dbo].[usp_test_Phone_Search]
+                                END";
+            return ExecuteDdlStatement(deleteScript);
+        }
+
+        #endregion
+    }
+}
